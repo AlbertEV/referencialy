4f3263d71423f8aa280c2149ae554b0f9f15cbc9
AlbertEV <39110818+AlbertEV@users.noreply.github.com>
2021-03-20 13:02:23 -0400

    init commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app" class="container">
+    <MyNavbar />
+
+    <router-view />
+  </div>
+</template>
+
+<script>
+import MyNavbar from "./views/navbar.vue";
+export default {
+  components: { MyNavbar },
+};
+</script>
